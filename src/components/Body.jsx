a2bab31e48a873862bc73117ba29e66e5b287a82
Ashutosh Narayan <ashutosh75jsr@gmail.com>
2025-08-04 04:33:13 +0530

    commit

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,17 @@
 import Login from "./login";
 import Browse from "./Browse";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -13,6 +22,18 @@ const Body = () => {
       element: <Browse />,
     },
   ]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName } = user;
+        dispatch(addUser({ uid: uid, name: displayName, email: email }));
+        navigate("/browse");
+      } else {
+        dispatch(removeUser());
+      }
+    });
+  }, []);
 
   return (
     <div>
